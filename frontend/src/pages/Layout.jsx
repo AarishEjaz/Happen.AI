aabcc737241752b46d18fb1737d4e57d88f323f3
AarishEjaz <aarishejaz@gmail.com>
2025-10-15 11:44:37 +0530

    added routes

---
+++ frontend/src/pages/Layout.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const Layout = () => {
+  return (
+    <div>
+      
+      <h1>Layout</h1>
+      <Outlet />      
+    </div>
+  )
+}
+
+export default Layout
+
+
+// The Outlet component in react-router-dom is a crucial element for rendering nested routes and managing layouts within your React application. It acts as a placeholder where child routes of a parent route will be rendered.
+
+// Key functions of Outlet:
+// Nested Route Rendering: When you define a parent route with child routes, the Outlet component within the parent's element determines where those child routes will be displayed.
+
+// Layout Management: Outlet facilitates the creation of shared layouts. You can define a layout component (e.g., a header, sidebar, and footer) and place an Outlet within it. Then, all child routes of that layout will render their specific content within the Outlet, while sharing the common layout elements.
+
+// Dynamic Content Display: It dynamically renders the content of the matching child route based on the current URL. If no child route matches, Outlet renders nothing.
