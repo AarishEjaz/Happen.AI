b486b11e493f65a92fac53c525fc78c6f66ab78b
AarishEjaz <aarishejaz@gmail.com>
2025-10-17 16:47:57 +0530

    added plan

--- frontend/src/components/Plan.jsx
+++ frontend/src/components/Plan.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {PricingTable}  from '@clerk/clerk-react'
 
 const Plan = () => {
   return (
@@ -8,6 +9,10 @@ const Plan = () => {
             Choose Your Plan
         </h2>
         <p className='text-gray-500 max-w-lg mx-auto'> Start for free and scale up as you grow. Find the perfect plan for your content creation needs.</p>
+      </div>
+      <div className='mt-14 max-sm:mx-8'>
+        <PricingTable />
+        
       </div>
 
     </div>
