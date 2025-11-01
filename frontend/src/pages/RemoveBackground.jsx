4144dec4e8e171a11c57664298c4dbaa058fcd12
AarishEjaz <aarishejaz@gmail.com>
2025-11-01 18:35:49 +0530

    changed removeBackground page

--- frontend/src/pages/RemoveBackground.jsx
+++ frontend/src/pages/RemoveBackground.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Eraser, Sparkles, Hash} from 'lucide-react'
 
 const RemoveBackground = () => {
 
@@ -12,49 +13,32 @@ const RemoveBackground = () => {
       {/* left col */}
       <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
         <div className='flex items-center gap-3'>
-          <Sparkles className='w-6 text-[#00AD25]'/>
-          <h1 className='text-xl font-semibold'>AI Image Generator</h1>
+          <Sparkles className='w-6 text-[#FF4938]'/>
+          <h1 className='text-xl font-semibold'>Background Removal</h1>
         </div>
-        <p className='mt-6 text-sm font-medium'>Describe Your Image</p>
-        <textarea onChange={(e)=>setInput(e.target.value)} value={input} rows={4}  className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300' 
-        placeholder='Describe what do you want to see in the image' required/>
-        <p className='mt-4 text-sm font-medium'>Style</p>
-        <div className='mt-3 flex gap-3 flex-wrap sm:max-w-9/11'>
-          {
-            imageStyle.map((item)=>(
-              <span onClick={()=>setSelectedStyle(item)}  
-              className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${selectedStyle===item?'bg-green-50 text-green-700':'text-gray-500 border-gray-300'}`} key={item} >{item}</span>
-            ))
-          }
-        </div>
-        <div className='my-6 flex items-center gap-2'>
-          <label className='relative cursor-pointer'>
-             <input type="checkbox" onChange={(e)=>setPublish(e.target.checked)} checked={publish} className='sr-only peer' />
-             <div className='w-9 h-5 bg-slate-300 rounded-full peer-checked:bg-green-500 transition'></div>
-             <span className='absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition peer-checked:translate-x-4'></span>
-          </label>
-          <p className='text-sm'>Make this image Public</p>
-        </div>
-        
+        <p className='mt-6 text-sm font-medium'>Upload image</p>
+        <input onChange={(e)=>setInput(e.target.files[0])} type="file" accept='image/*' className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-600'  required/>
+         <p className='text-xs text-gray-500 font-light mt-1'>Support JPG, PNG and other image formats</p> 
+
         <button className='w-full flex justify-center items-center gap-2 
-        bg-gradient-to-r from-[#00AD25] to-[#04FF50] 
+        bg-gradient-to-r from-[#F6AB41] to-[#FF4938] 
         text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
-          <Image className='w-5' />
-          Generate Image
+          <Eraser className='w-5' />
+          Remove Background
         </button>
       </form>
 
       {/* right col */}
       <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]'>
         <div className='flex items-center gap-3'>
-          <Image className='w-5 h-5 text-[#00AD25]'/>
-          <h1 className='text-xl font-semibold'> Generated Image</h1>
+          <Eraser className='w-5 h-5 text-[#FF4938]'/>
+          <h1 className='text-xl font-semibold'> Processed Image</h1>
         </div>
 
         <div className='flex-1 flex justify-center items-center'>
           <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
-            <Image className='w-9 h-9' />
-            <p>Enter a topic and click "Generate Image" to get started</p>
+            <Eraser className='w-9 h-9' />
+            <p>Upload an image and click "Remove Background" to get started</p>
           </div>
         </div>
       </div>
