346020f267fe3ef3555d7d378d59975e052d0a26
AarishEjaz <aarishejaz@gmail.com>
2025-11-10 11:06:08 +0530

    added middleware for checking user plan

---
+++ server/middleware.js/auth.js
@@ -0,0 +1,27 @@
+//middleware to check User id and has premium plan 
+
+import { clerkClient } from "@clerk/express"
+
+export const auth = async(req, res) =>{
+    try{
+        const {userId, has} = await req.auth()
+        const hasPremiumPlan = await has({plan:'premium'})
+
+        const user = await clerkClient.users.getUser(userId)
+        if(!hasPremiumPlan && user.privateMetadata.free_usage){
+            request.free_usage = user.privateMetadata.free_usage
+        }else{
+            await clerkClient.users.updateUserMetadata(userId,{
+                privateMetadata: {
+                    free_usage:0
+                }
+            })
+            req.free_usage = 0
+
+        }
+        req.plan = hasPremiumPlan?'premium':'free'
+        next()
+    }catch(error){
+        res.json({success:false, message:error.message})
+    }
+}
