b37bbe9cee7fcc57b2b27e2861c8d7fcb01a0b16
AarishEjaz <aarishejaz@gmail.com>
2025-11-13 10:09:44 +0530

    added blog title and image generation controller

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -3,6 +3,7 @@ import { GoogleGenAI } from "@google/genai";
 import sql from '../configs/db.js'
 import { clerkClient,clerkMiddleware, getAuth } from "@clerk/express";
 import { configDotenv } from "dotenv";
+import { response } from "express";
 
 // const openai = new OpenAI({
 //     apiKey: process.env.GEMINI_API_KEY
@@ -15,7 +16,8 @@ const ai = new GoogleGenAI(process.env.GEMINI_API_KEY);
 
 export const generateArticle = async(req, res) =>{
     try{
-        const {userId} = getAuth(req)
+        // console.log("testing 1")
+        const {userId} =  getAuth(req)
         const {prompt, length} = req.body
         const plan = req.plan
         const free_usage = req.free_usage
@@ -39,9 +41,9 @@ export const generateArticle = async(req, res) =>{
         //     max_tokens: length
         // });
 
-        console.log("testing 3")
+        // console.log("testing 3")
           const response = await ai.models.generateContent({
-              model: "gemini-2.5-flash",
+              model: "gemini-2.0-flash",
               contents: `Generate an article on ${prompt} in about ${length} words`,
             });
             // console.log(response.text)
@@ -54,7 +56,7 @@ export const generateArticle = async(req, res) =>{
         await sql`INSERT INTO creations (user_id, prompt,content, type)
         VALUES(${userId},${prompt}, ${content}, 'article' )`
 
-        console.log("testing 4")
+        // console.log("testing 4")
           console.log(userId)
 
         if(plan !='premium'){
@@ -65,13 +67,111 @@ export const generateArticle = async(req, res) =>{
             })
         }
 
-        console.log("testing 5")
+        // console.log("testing 5")
         res.json({success:true, content})
 
     }catch(error){
-        // console.log("hello world")
-        // console.log(req.auth)
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
+}
+// ----------------------------------------BLOG TITLE ------------------------------------------------------------
+export const generateBlogTitle = async(req, res) =>{
+    try{
+        // console.log("testing 1")
+        const {userId} =  getAuth(req)
+        const {prompt,publish} = req.body
+        const plan = req.plan
+        const free_usage = req.free_usage
+
+        if(plan !=='premium'){
+            return res.json({success:false, message:'This feature is only available for premium subscriptions.'})
+        }
+
+
+        // console.log("testing 3")
+          const response = await ai.models.generateContent({
+              model: "gemini-2.0-flash",
+              contents: `${prompt}`,
+            });
+
+
+            
+
+            const content = response.text
+          
+
+        await sql`INSERT INTO creations (user_id, prompt,content, type)
+        VALUES(${userId},${prompt}, ${content}, 'blog-title' )`
+
+        // console.log("testing 4")
+          console.log(userId)
+
+        if(plan !='premium'){
+            await clerkClient.users.updateUserMetadata(userId,{
+                privateMetadata:{
+                    free_usage: free_usage + 1
+                }
+            })
+        }
+
+        // console.log("testing 5")
+        res.json({success:true, content})
+
+    }catch(error){
+        console.log(error)
         res.json({success:false, message:error.message})
     }
 }
 
+// -------------------------------------------------------Generate Image-------------------------------------------------------
+export const generateImage = async(req, res) =>{
+    try{
+        // console.log("testing 1")
+        const {userId} =  getAuth(req)
+        const {prompt} = req.body
+        const plan = req.plan
+        const free_usage = req.free_usage
+
+        if(plan !=='premium'){
+            return res.json({success:false, message:'This feature is only available for premium subscriptions.'})
+        }
+
+
+        // console.log("testing 3")
+
+        const formData = new FormData()
+        formData.append('prompt', prompt )
+            
+        await axios.post('https://clipdrop-api.co/text-to-image/v1',formData, {
+            headers:{'x-api-key': YOUR_API_KEY,},
+            responseType: "arrayBuffer"
+        })
+        .then(response => response.arrayBuffer())
+        .then(buffer => {
+        // buffer here is a binary representation of the returned image
+        })
+          
+
+        await sql`INSERT INTO creations (user_id, prompt,content, type)
+        VALUES(${userId},${prompt}, ${content}, 'blog-title' )`
+
+        // console.log("testing 4")
+          console.log(userId)
+
+        if(plan !='premium'){
+            await clerkClient.users.updateUserMetadata(userId,{
+                privateMetadata:{
+                    free_usage: free_usage + 1
+                }
+            })
+        }
+
+        // console.log("testing 5")
+        res.json({success:true, content})
+
+    }catch(error){
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
+}
--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,27 +1,67 @@
+// //middleware to check User id and has premium plan 
+
+// import { clerkClient,clerkMiddleware, getAuth } from "@clerk/express"
+
+// export const auth = async(req, res,next) =>{
+//     try{
+//         const {userId, has} =  getAuth(req)
+//         const hasPremiumPlan = await has({plan:'premium'})
+
+//         const user = await clerkClient.users.getUser(userId)
+//         if(!hasPremiumPlan && user.privateMetadata.free_usage){
+//             req.free_usage = user.privateMetadata.free_usage
+//         }else{
+//             await clerkClient.users.updateUserMetadata(userId,{
+//                 privateMetadata: {
+//                     free_usage:0
+//                 }
+//             })
+//             req.free_usage = 0
+
+//         }
+//         req.plan = hasPremiumPlan?'premium':'free'
+//         next()
+//     }catch(error){
+//         res.json({success:false, message:error.message})
+//     }
+// }
+
+
 //middleware to check User id and has premium plan 
 
-import { clerkClient,clerkMiddleware } from "@clerk/express"
+import { clerkClient, getAuth } from "@clerk/express"
 
-export const auth = async(req, res,next) =>{
+export const auth = async(req, res, next) => {
     try{
-        const {userId, has} = await req.auth()
-        const hasPremiumPlan = await has({plan:'premium'})
+        const { userId } = getAuth(req)
+        
+        if(!userId) {
+            return res.status(401).json({
+                success: false, 
+                message: 'Unauthorized'
+            })
+        }
 
         const user = await clerkClient.users.getUser(userId)
-        if(!hasPremiumPlan && user.privateMetadata.free_usage){
-            request.free_usage = user.privateMetadata.free_usage
-        }else{
-            await clerkClient.users.updateUserMetadata(userId,{
+        const plan = user.publicMetadata?.plan || 'free'
+        const hasPremiumPlan = plan === 'premium'
+        
+        if(!hasPremiumPlan && user.privateMetadata?.free_usage){
+            req.free_usage = user.privateMetadata.free_usage
+        } else if(!hasPremiumPlan) {
+            await clerkClient.users.updateUserMetadata(userId, {
                 privateMetadata: {
-                    free_usage:0
+                    free_usage: 0
                 }
             })
             req.free_usage = 0
-
         }
-        req.plan = hasPremiumPlan?'premium':'free'
+        
+        req.plan = plan
         next()
-    }catch(error){
-        res.json({success:false, message:error.message})
+        
+    } catch(error) {
+        console.error('Auth middleware error:', error)
+        res.status(500).json({success: false, message: error.message})
     }
 }
